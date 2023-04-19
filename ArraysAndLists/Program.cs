7e177421f3844f6b82fb4607cf6ecde60ebc3aa8
Rakin Brown <rakinbrown38@gmail.com>
2023-04-19 10:59:11 -0500

    even and odd loop

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -8,12 +9,24 @@ namespace ArraysAndLists
         {
             //TODO:
 
-            // Create an int Array and populate numbers 1-10
+            var myNymbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };// Create an int Array and populate numbers 1-10
+            var oddNumbers = new List<int>() { 1, 3, 5, 7, 9 };/* Name one List "evens"*/
+            //* Name the other List "odds"
+            var evenNumbers = new List<int>() { 2, 4, 6, 8, 10 };/* Create two Lists of type int.*/
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            foreach(var num in myNymbers)
+            {
+                if (num %2 == 0)
+                {
+                    evenNumbers.Add(num);
+                    Console.WriteLine($"{num} is an even number!");
+                }
+                else
+                {
+                    oddNumbers.Add(num);
+                    Console.WriteLine($"{num} is an odd number!");
+                }
+            }
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
